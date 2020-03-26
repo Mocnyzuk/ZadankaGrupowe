60f3a76d5586b3db894a1c9013ff8ea60e55eacb
TheDankMatter <piotriwo@gmail.com>
2020-03-26 14:38:12 +0100

    Added altPair()

--- src/main/java/A3/Zadania_A3_2.java
+++ src/main/java/A3/Zadania_A3_2.java
@@ -7,8 +7,21 @@ public class Zadania_A3_2 {
         return str.substring(0, n) + str.substring(n + 1, str.length());
     }
 
+    //Warmup-1 > backAround
     public static String backAround(String str) {
         return str.charAt(str.length() - 1) + str + str.charAt(str.length() - 1);
     }
 
+    //Warmup-2 >
+    public static String altPairs(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i += 4) {
+            int end = i + 2;
+            if (end > str.length()) {
+                end = str.length();
+            }
+            result = result + str.substring(i, end);
+        }
+        return result;
+    }
 }
