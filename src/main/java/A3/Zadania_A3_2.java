c06ca8ac6e6f422bf8314a24166542d93cd9a23b
TheDankMatter <piotriwo@gmail.com>
2020-03-26 14:14:49 +0100

    Implemented missingChar()

--- src/main/java/A3/Zadania_A3_2.java
+++ src/main/java/A3/Zadania_A3_2.java
@@ -1,6 +1,8 @@
 package A3;
 
 public class Zadania_A3_2 {
+
+    //Warmup-1 > missingChar
     public static String missingChar(String str, int n) {
         return str.substring(0, n) + str.substring(n + 1, str.length());
     }
