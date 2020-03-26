902acbb3e4679089ef6fce54313036f735af9866
kkaniap <xkaniax@gmail.com>
2020-03-26 14:10:46 +0100

    startHi + test

--- src/main/java/A3/Zadania_A3_4.java
+++ src/main/java/A3/Zadania_A3_4.java
@@ -2,4 +2,9 @@ package A3;
 
 public class Zadania_A3_4 {
 
+    //Warmup-1 > startHi
+    public static boolean startHi(String text){
+        String tmpText = text.substring(0,2).toLowerCase();
+        return tmpText.equals("hi");
+    }
 }
--- src/test/java/A3/Zadania_A3_4Test.java
+++ src/test/java/A3/Zadania_A3_4Test.java
@@ -1,7 +1,18 @@
 package A3;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Zadania_A3_4Test {
 
+    @Test
+    void startHi() {
+        //given
+        String text = "Hi Adam";
+        //when
+        boolean isStartHI = Zadania_A3_4.startHi(text);
+        //then
+        assertTrue(isStartHI);
+    }
 }
