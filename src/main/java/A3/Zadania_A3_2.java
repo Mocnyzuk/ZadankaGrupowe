b6b59a4435ebf7bcb31a07c808f36f60018b991a
TheDankMatter <piotriwo@gmail.com>
2020-03-26 15:27:08 +0100

    Added commonEnd()

--- src/main/java/A3/Zadania_A3_2.java
+++ src/main/java/A3/Zadania_A3_2.java
@@ -45,5 +45,14 @@ public class Zadania_A3_2 {
         return false;
     }
 
+    //Array-1 > commonEnd
+    public static boolean commonEnd(int[] a, int[] b) {
+        if (a[0] == b[0] || a[a.length - 1] == b[b.length - 1]){
+            return true;
+        }
+        return false;
+    }
+
+
 
 }
