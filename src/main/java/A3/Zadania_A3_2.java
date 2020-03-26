eb9def7b3026c2f07e780781ede2ad4c8488e045
TheDankMatter <piotriwo@gmail.com>
2020-03-26 15:41:01 +0100

    Added unlucky1()

--- src/main/java/A3/Zadania_A3_2.java
+++ src/main/java/A3/Zadania_A3_2.java
@@ -53,6 +53,14 @@ public class Zadania_A3_2 {
         return false;
     }
 
+    //Array-1 > unlucky1
+    public boolean unlucky1(int[] nums) {
+        if ((nums[0] == 1 && nums[1] == 3) || (nums[nums.length - 2] == 1 && nums[nums.length - 1] == 3)){
+            return true;
+        }
+        return false;
+    }
+
 
 
 }
