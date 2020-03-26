6a4bdf971c1e91ae9abfee326d763ef1da479972
TheDankMatter <piotriwo@gmail.com>
2020-03-26 14:13:50 +0100

    Added tests for missingChar()

---
+++ src/test/java/A3/Zadania_A3_2Test.java
@@ -0,0 +1,20 @@
+package A3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Zadania_A3_2Test {
+
+    @Test
+    @DisplayName("missingChar(\"kitten\", 1) → \"ktten\"")
+    void missingChar() {
+        // Given
+        String str = "kitten";
+        // When
+        // Then
+        assertEquals("ktten", Zadania_A3_2.missingChar("kitten",1));
+    }
+
+}
