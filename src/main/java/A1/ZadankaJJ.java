779b574207f3b6861e368ece8f1645fc00427045
Jakub Jureczko <jakub.jureczko@interia.pl>
2020-03-26 13:29:45 +0100

    Warmup -2 stringX metoda
    Warmup -2 stringX  2 testy
    
    Warmup -2 stringSplosion  metoda
    
    Warmup -2 stringSplosion  2 testy
    
    Logic-1 less20 metoda
    
    Logic-1 less20 2 testy
    
    Logic-1 fizzString2 metoda
    
    Logic-1 fizzString2 2 testy
    
    Logic -2 luckySum metoda
    
    Logic -2 luckySum 2 testy
    
    Logic -2 closeFar metoda
    
    Logic -2 closeFar 2 testy
    
    Logic -2 makeChocolate metoda
    
    Logic -2 makeChocolate 2 testy
    
    Array-1 hass23 metoda
    
    Array-1 hass23 2 testy
    
    Array-1 biggerTwo metoda
    
    Array-1 biggerTwo 2 testy
    
    Array-1 frontTwo metoda
    
    Array-1 frontTwo 2 testy
    
    Array-1 swapEnds metoda
    
    Array-1 swapEnds 2 testy

--- src/main/java/A1/ZadankaJJ.java
+++ src/main/java/A1/ZadankaJJ.java
@@ -1,32 +1,43 @@
 package A1;
 
+import java.util.Arrays;
+
 public class ZadankaJJ {
 
-    public static int makeChocolate(int small, int big,  int goal){
-        int reszta = goal/5;
-        if(reszta>big){
-            return -1;
-        }else if(reszta<small){
-            return reszta;
+    public static int makeChocolate(int small, int big, int goal) {
+        int counter = 0;
+        while (goal >= 5 && big >= 1) {
+            goal = goal - 5;
+            big -= 1;
         }
-        return -1;
+        while (goal >= 1 && small >= 1) {
+            goal -= 1;
+            small -= 1;
+            counter++;
+        }
+        if (goal > 0) {
+            return -1;
+        }
+        return counter;
     }
 
-    public static boolean closeFar(int a, int b, int c){
+    public static boolean closeFar(int a, int b, int c) {
         a = Math.abs(a);
         b = Math.abs(b);
         c = Math.abs(c);
 
-
-        if((b+1==a || b-1==a) && (c>=a+2 || c<=a+2)){
+        if (((b - 1) == a || (b + 1) == a || b == a) && ((a >= (c + 2) || a <= (c - 2)) && (b >= (c + 2) || b <= (c - 2)))) {
             return true;
+        } else if (((c - 1) == a || (c + 1) == a || c == a) && ((a >= (b + 2) || a <= (b - 2)) && (b >= (c + 2) || b <= (c - 2)))) {
+            return true;
+        }
+        return false;
 
-        }else return (c + 1 == a || c - 1 == a) && (b >= a + 2 || b <= a + 2);
     }
 
 
-    public static boolean arrayFront9(int[] nums){
-        if(nums.length<4) {
+    public static boolean arrayFront9(int[] nums) {
+        if (nums.length < 4) {
             for (int num : nums) {
                 if (num == 9) {
                     return true;
@@ -35,10 +46,9 @@ public class ZadankaJJ {
 
             }
             return false;
-        }
-        else {
-            for (int i=0; i<4 ;i++) {
-                if(nums[i] == 9){
+        } else {
+            for (int i = 0; i < 4; i++) {
+                if (nums[i] == 9) {
                     return true;
                 }
             }
@@ -48,13 +58,100 @@ public class ZadankaJJ {
 
     }
 
-    public static String stringX(String str){
-            char start = str.charAt(1);
-            char end = str.charAt(str.length()-1);
-            s = str.substring(1, str.length()-2);
-            while(str.contains("x")){
-                str.replace('x', );
-        };
+    public static String stringX(String str) {
+        if (str.length() <= 1) {
+            return str;
+
+        } else {
+            char start = str.charAt(0);
+            char end = str.charAt(str.length() - 1);
+            return start + str.substring(1, str.length() - 1).replaceAll("x", "") + end;
+        }
     }
 
+    public static String stringSplosion(String str) {
+        char[] znaki = str.toCharArray();
+        String solution = "";
+        for (int i = 0; i < znaki.length; i++) {
+            for (int y = 0; y <= i; y++) {
+                solution += znaki[y];
+            }
+        }
+        return solution;
+    }
+
+    public static boolean less20(int n) {
+        if (n % 20 == 0) {
+            return false;
+        } else if ((n + 2) % 20 == 0 || (n + 1) % 20 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static String fizzString2(int n) {
+        String result = n + "!";
+        if (n % 3 == 0) {
+            result = "Fizz!";
+        } else if (n % 5 == 0) {
+            result = "Buzz!";
+        }
+        if (n % 3 == 0 && n % 5 == 0) {
+            result = "FizzBuzz!";
+        }
+        return result;
+
+    }
+
+    public static int luckySum(int a, int b, int c) {
+        if (a != 13 && b != 13 && c != 13) {
+            return a + b + c;
+        } else if (a == 13) {
+            return 0;
+        } else if (b == 13) {
+            return a;
+        } else if (c == 13) {
+            return a + b;
+        }
+        return 0;
+    }
+
+    public static boolean has23(int[] nums) {
+        for (int num : nums) {
+            if (num == 2 || num == 3) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int[] biggetTwo(int[] a, int[] b) {
+        int sumaa = 0;
+        int sumab = 0;
+        for (int i = 0; i < 2; i++) {
+            sumaa += a[i];
+            sumab += b[i];
+        }
+        if (sumaa > sumab) {
+            return a;
+        } else if (sumaa < sumab) {
+            return b;
+        } else return a;
+    }
+
+    public static int[] frontPiece(int[] nums) {
+        int endIndex = nums.length ;
+        if (endIndex >= 2) {
+            endIndex = 2;
+        }
+        return Arrays.copyOfRange(nums, 0, endIndex);
+    }
+
+    public static int[] swapEnds(int[] nums){
+        int lastNum = nums[nums.length-1];
+        int firstNum = nums[0];
+        nums[0] = lastNum;
+        nums[nums.length-1]=firstNum;
+        return nums;
+    }
 }
--- src/test/java/A1/ZadankaJJTest.java
+++ src/test/java/A1/ZadankaJJTest.java
@@ -2,6 +2,8 @@ package A1;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ZadankaJJTest {
@@ -14,4 +16,114 @@ class ZadankaJJTest {
     void arrayFront9assertFalse() {
         assertFalse(ZadankaJJ.arrayFront9(new int[]{1, 2, 3, 4, 9}));
     }
+
+    @Test
+    void stringX() {
+        assertEquals("xHix", ZadankaJJ.stringX("xxHxix"));
+    }
+    @Test
+    void stringXDwa() {
+        assertEquals("abcd", ZadankaJJ.stringX("abxxxcd"));
+    }
+
+    @Test
+    void stringSplosion() {
+        assertEquals("CCoCodCode", ZadankaJJ.stringSplosion("Code"));
+    }
+    @Test
+    void stringSplosionDwa() {
+            assertEquals("aababc", ZadankaJJ.stringSplosion("abc"));
+        }
+
+    @Test
+    void less20() {
+        assertTrue(ZadankaJJ.less20(18));
+    }
+    @Test
+    void less20d() {
+        assertFalse(ZadankaJJ.less20(20));
+    }
+
+    @Test
+    void fizzString2() {
+        assertEquals("1!", ZadankaJJ.fizzString2(1));
+    }
+    @Test
+    void fizzString22() {
+        assertEquals("Fizz!", ZadankaJJ.fizzString2(3));
+    }
+
+    @Test
+    void luckySum() {
+        assertEquals(6, ZadankaJJ.luckySum(1, 2, 3));
+    }
+    @Test
+    void luckySum2() {
+        assertEquals(3, ZadankaJJ.luckySum(1, 2, 13));
+    }
+
+    @Test
+    void closeFar() {
+        assertTrue(ZadankaJJ.closeFar(1, 2,10));
+    }
+    @Test
+    void closeFa2r() {
+        assertFalse(ZadankaJJ.closeFar(1, 2,3));
+    }
+
+    @Test
+    void makeChocolate() {
+        assertEquals(4, ZadankaJJ.makeChocolate(4, 1, 9));
+    }
+
+    @Test
+    void makeChocolat2e() {
+        assertEquals(-1, ZadankaJJ.makeChocolate(4, 1, 10));
+    }
+
+    @Test
+    void has23() {
+        assertTrue(ZadankaJJ.has23(new int[] {2, 5}));
+    }
+    @Test
+    void has223() {
+        assertFalse(ZadankaJJ.has23(new int[]{4, 5}));
+    }
+
+    @Test
+    void biggetTwso() {
+        int[] a = new int[]{1,2};
+        int[]b = new int[]{3, 4};
+        assertEquals(b, ZadankaJJ.biggetTwo(a,b ));
+    }
+    @Test
+    void biggetTwo() {
+        int[] a = new int[]{1,1};
+        int[]b = new int[]{1,2};
+        assertEquals(b, ZadankaJJ.biggetTwo(a, b));
+    }
+
+    @Test
+    void frontPiece() {
+        int[] nums = {1, 2, 3};
+        int endIndex = 2;
+        assertArrayEquals(Arrays.copyOfRange(nums, 0, endIndex), ZadankaJJ.frontPiece(nums));
+
+    }
+    @Test
+    void frontPiesce() {
+        int[] nums = {1};
+        int endIndex = 1;
+        assertArrayEquals(Arrays.copyOfRange(nums, 0, endIndex), ZadankaJJ.frontPiece(nums));
+    }
+
+    @Test
+    void swapEnds() {
+        assertArrayEquals(new int[]{4,2,3,1}, ZadankaJJ.swapEnds(new int[]{1, 2, 3, 4}));
+    }
+    @Test
+    void swapE2nds() {
+        assertArrayEquals(new int[]{3, 2, 1}, ZadankaJJ.swapEnds(new int[]{1, 2, 3}));
+    }
 }
+
