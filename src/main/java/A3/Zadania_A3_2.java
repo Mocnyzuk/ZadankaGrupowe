defd72d2464fad4e0a4af22102c440f1690174b4
TheDankMatter <piotriwo@gmail.com>
2020-03-26 14:10:31 +0100

    Added missingChar()

--- src/main/java/A3/Zadania_A3_2.java
+++ src/main/java/A3/Zadania_A3_2.java
@@ -1,6 +1,7 @@
 package A3;
 
 public class Zadania_A3_2 {
-
-
+    public static String missingChar(String str, int n) {
+        return str.substring(0, n) + str.substring(n + 1, str.length());
+    }
 }
