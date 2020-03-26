dc2bf76ab81462c46de4f7a401ce5d5ef13e442e
Natalia <nszarek284@gmail.com>
2020-03-26 14:02:32 +0100

    Pierwsze zadanko grupa A3

---
+++ src/main/java/A3/Zadania_A3_1.java
@@ -0,0 +1,15 @@
+package A3;
+
+public class Zadania_A3_1 {
+
+    //Warmup-1 > sleepIn
+
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if(!weekday || vacation)
+        {
+            return true;
+        }
+        return false;
+
+    }
+}
---
+++ src/test/java/A3/Zadania_A3_1Test.java
@@ -0,0 +1,16 @@
+package A3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Zadania_A3_1Test {
+
+
+    @Test
+    void sleepIn() {
+        //given
+        //when
+        //then
+    }
+}
